2677319a43c788a90e21d45e90522d36cba4c304
Jack McCallie <65779661+Jack-Mac-21@users.noreply.github.com>
2022-06-22 00:28:02 -0500

    animations

--- animation.js
+++ animation.js
@@ -3,10 +3,15 @@ const observer = new IntersectionObserver(entries =>{
   entries.forEach(entry => {
     if(entry.isIntersecting){
       console.log('intersection found')
-      document.querySelector(".animated")[0].classList.add("fadeInRight")
+      img_element = document.getElementById('image-to-appear')
+      img_element.classList.add("fadeInLeft")
+      img_element.style.opacity = 1
+      img_element = document.getElementById('p-to-appear')
+      img_element.classList.add("fadeInRight")
+      img_element.style.opacity = 1
     }
   });
 
 })
 
-observer.observe(document.querySelector('.transition-skills-to-projects'));
+observer.observe(document.querySelector(".projects-transition"));
